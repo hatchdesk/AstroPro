eb09a7c70a53070e9aee83b41bdeee1bfc964ff1
Ankit Rajput <kumarankit90653@gmail.com>
2024-07-18 17:28:10 +0530

    Astro pro Completed Work push

--- Web.Application/ViewModels/Admin/Service/ServiceToCreateViewModel.cs
+++ Web.Application/ViewModels/Admin/Service/ServiceToCreateViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace Web.Application.ViewModels.Admin.Service
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Application.ViewModels.Admin.Service
 {
     public class ServiceToCreateViewModel
     {
+        [Required(ErrorMessage ="Icon Url is Required")]
         public string? Icon { get; set; }
+
+        [Required(ErrorMessage = "Title is Required")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Content is Required")]
         public string Content { get; set; } = string.Empty;
     }
 
