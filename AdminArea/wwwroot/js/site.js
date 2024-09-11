20f14a93fd389edb5a8781a719523033206455c8
Ankit Rajput <kumarankit90653@gmail.com>
2024-09-11 16:25:34 +0530

    tinymce editor client api changes

--- AdminArea/wwwroot/js/site.js
+++ AdminArea/wwwroot/js/site.js
@@ -7,20 +7,7 @@ import { Datatable, initMDB } from "mdb-ui-kit";
 
 initMDB({ Datatable });
 
-    document.getElementById('DropDownCategory').addEventListener('change', function () {
-        var selectedCategory = this.value;
-        alert("Hello , i am changing")
-
-    document.getElementById('feesBox').style.display = 'block';
-
-        fetch(`https://localhost:7173/Admin/Fee/Detail/2=${encodeURIComponent(selectedCategory)}`)
-            .then(response => response.json())
-        .then(data => {
-
-        document.getElementById('feesAmount').innerText = `₹${data.fees}`;
-            })
-            .catch(error => console.error('Error fetching fees:', error));
-    });
+   
 
 
 
