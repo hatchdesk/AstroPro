d52d8622a5b289af167e8aea2c180e9d4be32177
isachin100 <isachin100@gmail.com>
2024-10-07 12:08:16 +0530

    changes

---
+++ AdminArea/CommonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Web
+{
+	public static class CommonHelper
+	{
+		public static string ExtractTextFromHtml(string? html)
+		{
+			if (html == null)
+			{
+				return "";
+			}
+
+			string plainText = Regex.Replace(html, "<[^>]+?>", " ");
+			plainText = System.Net.WebUtility.HtmlDecode(plainText).Trim();
+
+			return plainText;
+		}
+	}
+}
